70d0ee63b51e8fe812e756a3ce4788783034f3de
feurjarx <feurjarx@gmail.com>
2017-01-02 01:09:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+var fb = FB.make('#user-form', [{
+	name: 'method',
+	getter: function() {
+		return this.selectedOptions[0].dataset.value;
+	},
+	setter: function(v) {
+		
+		[].forEach.call(this.options, function(optionElem) {
+			if (optionElem.dataset.value === v) {
+				optionElem.selected = true;
+				return false;
+			}
+		});
+	}
+}]);
 
-var fb = FB.make('#user-form');
+// Create
+fb.create({
+	info: 'abc',
+	params: {
+		email: 'asds@dsad'
+	}
+};
+
+// Read
 console.table(fb.data);
+
+// Update
+fb.data = {
+	user: {
+		name: 'feurjarx'
+	},
+	method: '{"key": "meta_incapsulation", "text": "meta"}'
+};
+
+// Delete
+fb.clear();
+// or ...
+fb.data = null;
