f6b0af04ff478751c72fe9b86cc6367c2a2eae4b
feurjarx <feurjarx@gmail.com>
2017-01-01 20:19:15 +0600

    Create form-builder.js

---
+++ form-builder.js
@@ -0,0 +1,139 @@
+var FB = (function() {
+	
+	var formElem;
+	
+	function readFromField(field) {
+		
+		var result = {}
+		var parent = result;
+		var key;
+	
+		var parts = field.name.split('[');
+	
+		var startValue = {};
+		
+		parts.forEach(function(part, i){
+			
+			key = part.replace(']', '');
+
+			if (parts[i + 1] && parts[i + 1] === ']') {
+				startValue = [];
+			}
+			
+			if (i === parts.length - 1) {
+				
+				if (parent instanceof Array) {
+				
+					if (field.selectedOptions instanceof HTMLCollection) {
+					
+						[].forEach.call(field.selectedOptions, function(optionElem) {
+							parent.push(optionElem.value);
+						});
+					
+					} else {
+						
+						parent.push(field.value);
+					}
+				
+				} else {
+					
+					parent[key] = field.value;
+				}
+				
+			} else {
+				
+				parent[key] = parent[key] || Object.create(startValue);
+			}
+			
+			parent = parent[key];
+		});
+	
+		return result;
+	};
+	
+	function objectMerge() {
+		
+		var result = {};
+		for (var i = 0; i < arguments.length; i++) {
+			var obj = arguments[i];
+
+			if (!obj) {
+				continue;
+			}
+
+			for (var key in obj) {
+				if (obj.hasOwnProperty(key)) {
+					if (typeof obj[key] === 'object') {
+					  
+					  result[key] = objectMerge(result[key], obj[key]);
+					
+					} else {
+					  
+					  result[key] = obj[key];
+					}
+				}
+			}
+		}
+
+		return result;
+	};
+	
+	
+	return {
+		make: function(formSelector) {
+			
+			formElem = document.querySelector(formSelector);
+			
+			var fb = {
+				form: formElem,
+				// CREATE
+				// todo: make create elements by object
+				
+				// READ
+				get data() {
+					
+					var fields = formElem.querySelectorAll('[name]');
+					var data = {};
+					[].forEach.call(fields, function(field) { 
+						data = objectMerge(data, readFromField(field));
+					});
+					
+					return data;
+				},
+				readDataByFieldName: function(fieldName) {
+					var field = formElem.querySelector('[name="' + fieldName + '"]');
+					return readFromField(field);
+				},
+				readDataByFieldSelector: function(selector) {
+					var fields = formElem.querySelectorAll(selector);
+					var data = {};
+					[].forEach.call(fields, function(field) { 
+						data = objectMerge(data, readFromField(field));
+					});
+					
+					return data;
+				},
+				
+				// UPDATE
+				set data(v) {
+					
+					debugger
+				},
+				
+				updateDataByFieldName: function(v) {
+					
+					
+				},
+				
+				updateFieldByFieldName: function(v) {
+					
+					
+				},
+				
+				// DELETE 
+			};
+			
+			return fb;
+		}
+	}
+}());
